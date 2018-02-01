cb87a65df9dcdf1176ddf3d77956e57f09c6d2c9
FullFaceRub <danielhbury@gmail.com>
2018-01-31 17:00:51 -0700

    working on responsive

--- db/addproduct.sql
+++
@@ -1,3 +0,0 @@
-insert into products
-(name,description,price,image_url, category)
-values($1,$2,$3,$4)
--- db/newtransaction.sql
+++
@@ -1,3 +0,0 @@
-insert into transactions
-(customer_name, ccnumber, date, total)
-values ($1,$2,$3,$4)
